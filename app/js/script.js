5f717d5e83eee284f6b6a4c196db52d97cb9c6e0
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-01-06 11:03:21 +0600

    animation fixed

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,19 @@
 const header = document.querySelector(".header");
 const headerToggle = document.querySelector(".header__toggle");
 const overlay = document.querySelector(".overlay");
+const fadeElms = document.querySelectorAll(".has-fade");
 
 headerToggle.addEventListener("click", () => {
   header.classList.toggle("open");
   if (header.classList.contains("open")) {
-    overlay.classList.add("fade-in");
-    overlay.classList.remove("fade-out");
+    fadeElms.forEach((elm) => {
+      elm.classList.add("fade-in");
+      elm.classList.remove("fade-out");
+    });
   } else {
-    overlay.classList.remove("fade-in");
-    overlay.classList.add("fade-out");
+    fadeElms.forEach((elm) => {
+      elm.classList.add("fade-out");
+      elm.classList.remove("fade-in");
+    });
   }
 });
