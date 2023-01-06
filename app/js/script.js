a90b91a40cea8d51ee2f8e95f3bff7e3398603c8
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-01-06 11:12:52 +0600

    overflow hidded added when menu is open

--- app/js/script.js
+++ app/js/script.js
@@ -2,16 +2,23 @@ const header = document.querySelector(".header");
 const headerToggle = document.querySelector(".header__toggle");
 const overlay = document.querySelector(".overlay");
 const fadeElms = document.querySelectorAll(".has-fade");
-const body = document.getElementsByTagName("body");
+const body = document.querySelector("body");
 
 headerToggle.addEventListener("click", () => {
   header.classList.toggle("open");
+
+  //when the menu is open
   if (header.classList.contains("open")) {
     fadeElms.forEach((elm) => {
+      body.classList.add("noscroll");
       elm.classList.add("fade-in");
       elm.classList.remove("fade-out");
     });
-  } else {
+  }
+
+  //when the menu is close
+  else {
+    body.classList.remove("noscroll");
     fadeElms.forEach((elm) => {
       elm.classList.add("fade-out");
       elm.classList.remove("fade-in");
